75d4c6a09a305186e37ba6921b000a977b4973d6
kaifmalek17 <kaifmalek1751@.com>
2023-06-21 12:31:54 +0530

    day 12 completed

--- lib/models/catelog.dart
+++ lib/models/catelog.dart
@@ -1,17 +1,8 @@
-class CatelogModel {
-  static final List<Item> products = [
-    Item(
-      id: 1,
-      name: "iPhone 12 Pro",
-      desc: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://www.smartcellular.in/media/catalog/product/cache/cfaa1692031f009488d1cfea5ce7e1ee/a/p/apple-iphone-12-pro-max-dummyapplefsn-original-imafwgcyhmzhaapd_1.jpeg",
-    ),
-  ];
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
 
-  static List<Item> items = List.from(products);
+class CatelogModel {
+  static List<Item> items = [];
 }
 
 class Item {
@@ -30,37 +21,76 @@ class Item {
     required this.color,
     required this.image,
   });
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      desc: map['desc'] as String,
+      price: map['price'] as num,
+      color: map['color'] as String,
+      image: map['image'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) =>
+      Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.desc == desc &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        desc.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
+  }
 }
-
-
-// class CatelogModel {
-//   static final products = [
-//     Item(
-//         id: 1,
-//         name: "iPhone 12 Pro",
-//         desc: "Apple iPhone 12th generation",
-//         price: 999,
-//         color: "#33505a",
-//         image:
-//             "https://www.smartcellular.in/media/catalog/product/cache/cfaa1692031f009488d1cfea5ce7e1ee/a/p/apple-iphone-12-pro-max-dummyapplefsn-original-imafwgcyhmzhaapd_1.jpeg")
-//   ];
-
-//   static var items;
-// }
-
-// class Item {
-//   final int id;
-//   final String name;
-//   final String desc;
-//   final num price;
-//   final String color;
-//   final String image;
-
-//   Item(
-//       {required this.id,
-//       required this.name,
-//       required this.desc,
-//       required this.price,
-//       required this.color,
-//       required this.image});
-// }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:mine_app/models/catelog.dart';
 import 'package:mine_app/widgets/drawer.dart';
 import 'package:mine_app/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int num = 8;
+
   final String name = 'Kaif';
 
-  const HomePage({super.key});
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catelogJson =
+        await rootBundle.loadString('assets/files/catelog.json');
+    final decodedData = jsonDecode(catelogJson);
+    var productsData = decodedData['products'];
+    CatelogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +44,16 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatelogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatelogModel.items[index],
-            );
-          },
-        ),
+        child: (CatelogModel.items != null && CatelogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatelogModel.items.length,
+                itemBuilder: (context, index) => ItemWidget(
+                  item: CatelogModel.items[index],
+                ),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: const MyDrawer(),
     );
